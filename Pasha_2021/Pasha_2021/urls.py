2d28e66e825cd3fb88846484ce938a2189fb4112
pasha <pasha.kivachuk13@gmail.com>
2022-10-19 16:50:52 +0300

    start_files

--- Pasha_2021/Pasha_2021/urls.py
+++ Pasha_2021/Pasha_2021/urls.py
@@ -1,4 +1,4 @@
-"""projectdjango URL Configuration
+"""Pasha_2021 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.about),
-    path('home/', views.home),
-    path('reverse/', views.reverse, name ='reversed'),
+
 ]
